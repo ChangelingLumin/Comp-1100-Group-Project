a2d312263524a679dbd142cf9ff345de96051b59
unknown <curtis.brandstetter@student.sl.on.ca>
2018-10-15 17:53:09 -0400

    Just started us off, be sure to read the green text. as that's the main way for us to comunicate while working on the code.

---
+++ curtispaulgroupproject.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; // just so we dont have to use std:: all the time
+                     // it makes it look nicer
+
+int main() // just to start us off
+{
+    //this is the first commit with the code
+    //lets wait until we learn about switches in class
+    //before we add anything new
+    return 0;
+}
