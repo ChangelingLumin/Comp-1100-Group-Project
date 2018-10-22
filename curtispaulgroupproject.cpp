88b04f7ed59d1f5e60d87dc02133aef743875ce1
unknown <curtis.brandstetter@student.sl.on.ca>
2018-10-22 14:24:53 -0400

    Fleshed out the code a bit more

--- curtispaulgroupproject.cpp
+++ curtispaulgroupproject.cpp
@@ -4,8 +4,45 @@ using namespace std; // just so we dont have to use std:: all the time
 
 int main() // just to start us off
 {
-    //this is the first commit with the code
-    //lets wait until we learn about switches in class
-    //before we add anything new
+    int answeryn
+
+    switch (questions) 
+    {
+        case q1:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q2:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q3:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q4:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q5:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q6:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q7:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q8:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+        case q9:
+            cout << "question" << endl;
+            cin >> answeryn >> endl;
+            // the above code might not be correct, I will be talking with bryden about this later
+            // But from what I saw on learncpp.com it looks correct.
+            // please put in the questions as I forgot them. Don't worry about the answers.
+            // I'll be putting in some if statements once I learn how to combine both case and if statements.
+            // We still have alot of time to complete the project, so don't worry too much about getting it done.
+            // Let's just work on getting the skeleten of the code finished, and we will debug it after reading week.
+            // if you have any comments about the code, please use the comment function for vs code as it is easeir to read.
+            // just be sure to place it on the line that you want edited.
+    }
     return 0;
 }
