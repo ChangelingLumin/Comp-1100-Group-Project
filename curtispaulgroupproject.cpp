d1f4fd1f9e51b84aeaa21f669146f664fe316871
unknown <curtis.brandstetter@student.sl.on.ca>
2018-10-24 02:57:34 -0400

    That test fixed the code thanks

--- curtispaulgroupproject.cpp
+++ curtispaulgroupproject.cpp
@@ -4,57 +4,116 @@ using namespace std; // just so we dont have to use std:: all the time
 
 int main()  // just to start us off
 { 
-    int answeryn ;
+    char input = 'y';
 
-    switch (questions) 
+cout << "1. Are there similar product’s " << 
+        "companies less than three?"   << endl;
+cin  >> input ;
+    switch (input) 
     {
-        case q1:
-            cout << "Are there similar product’s " << 
-                    "companies less than three?"   << endl;
-            cin  >> answeryn ;
+        case 'y': 
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
 
-        case q2:
-            cout << "Does this product target "  <<
-                    "for all generations?"       << endl;
-            cin  >> answeryn ;
-
-        case q3:
-            cout << "Does it have competitive price "         << 
-                    "than other companies selling in Canada?" << endl;
-            cin  >> answeryn ;
-
-        case q4:
-            cout << "Does it have better quality " <<
-                    "than others?"                 << endl;
-            cin  >> answeryn ;
-
-        case q5:
-            cout << "Is it for four season? " << endl;
-            cin  >> answeryn ;
-
-        case q6:
-            cout << "Is stock management easy? " << endl;
-            cin  >> answeryn ;
-
-        case q7:
-            cout << "question" << endl;
-            cin >> answeryn;
-
-        case q8:
-            cout << "question" << endl;
-            cin >> answeryn;
-
-        case q9:
-            cout << "question" << endl;
-            cin >> answeryn;
-            // the above code might not be correct, I will be talking with bryden about this later
-            // But from what I saw on learncpp.com it looks correct.
-            // please put in the questions as I forgot them. Don't worry about the answers.
-            // I'll be putting in some if statements once I learn how to combine both case and if statements.
-            // We still have alot of time to complete the project, so don't worry too much about getting it done.
-            // Let's just work on getting the skeleten of the code finished, and we will debug it after reading week.
-            // if you have any comments about the code, please use the comment function for vs code as it is easeir to read.
-            // just be sure to place it on the line that you want edited.
+        default: 
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
     }
+cout << "2. Does this product target "  <<
+        "for all generations?"       << endl;
+cin  >> input ;
+
+    switch (input) 
+    {
+        case 'y':
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
+
+        default:
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
+    }
+
+cout << "3. Does it have competitive price "      << 
+        "than other companies selling in Canada?" << endl;
+cin  >> input ;
+
+    switch (input) 
+    {
+        case 'y':
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
+
+        default:
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
+    }
+cout << "4. Does it have better quality " <<
+        "than others?"                    << endl;
+cin  >> input ;
+    switch (input) 
+    {
+        case 'y':
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
+
+        default:
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
+    }
+
+cout << "5. Is it for four season? " << endl;
+cin  >> input ;
+
+    switch (input) 
+    {
+        case 'y':
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
+
+        default:
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
+    }
+
+cout << "6. Is stock management easy? " << endl;
+cin  >> input ;
+    switch (input) 
+    {
+        case 'y':
+        cout << "************" << endl;
+        cout << "****    ****" << endl;
+        cout << "** Buy it **" << endl; 
+        cout << "****    ****" << endl;
+        cout << "************" << endl;
+        break;
+
+        default:
+        cout << "***************" << endl;
+        cout << "** Don't Buy **" << endl; 
+        cout << "***************" << endl;
+        return 0;
+    }
+
     return 0;
 }
