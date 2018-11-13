dba3f3939264ba2f602c3df1c0f946f8ce04d8d9
Seonghyun Choi <43017312+kg41csh@users.noreply.github.com>
2018-11-12 22:12:57 -0500

    update

--- workingtest.cpp
+++ workingtest.cpp
@@ -4,6 +4,9 @@ using namespace std; // just so we dont have to use std:: all the time
 
 int main()  // just to start us off
 { 
+
+// Level 1
+
     char input = 'y';
 
 cout << "1. Are there similar product’s " << 
@@ -12,12 +15,16 @@ cin  >> input ;
     switch (input) 
     {
         case 'y': 
+        case 'Y':
+
+        cout << endl;
         cout << "*************************" << endl;
         cout << "Answer the next question." << endl;
         cout << "*************************" << endl;
         break;
 
         default: 
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
@@ -30,12 +37,16 @@ cin  >> input ;
     switch (input) 
     {
         case 'y':
+        case 'Y':
+
+        cout << endl;        
         cout << "*************************" << endl;
         cout << "Answer the next question." << endl;
         cout << "*************************" << endl;
         break;
 
         default:
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
@@ -49,12 +60,16 @@ cin  >> input ;
     switch (input) 
     {
         case 'y':
+        case 'Y':
+
+        cout << endl;
         cout << "*************************" << endl;
         cout << "Answer the next question." << endl;
         cout << "*************************" << endl;
         break;
 
         default:
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
@@ -66,12 +81,15 @@ cin  >> input ;
     switch (input) 
     {
         case 'y':
+        case 'Y':
+        cout << endl;
         cout << "*************************" << endl;
         cout << "Answer the next question." << endl;
         cout << "*************************" << endl;
         break;
 
         default:
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
@@ -84,12 +102,15 @@ cin  >> input ;
     switch (input) 
     {
         case 'y':
+        case 'Y':
+        cout << endl;
         cout << "*************************" << endl;
         cout << "Answer the next question." << endl;
         cout << "*************************" << endl;
         break;
 
         default:
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
@@ -101,19 +122,69 @@ cin  >> input ;
     switch (input) 
     {
         case 'y':
-        cout << "************" << endl;
-        cout << "****    ****" << endl;
-        cout << "** Buy it **" << endl; 
-        cout << "****    ****" << endl;
-        cout << "************" << endl;
+        case 'Y':
+        cout << endl;
+        cout << "*************************" << endl;
+        cout << "Answer the next question." << endl;
+        cout << "*************************" << endl;
+        break;
         break;
 
         default:
+        cout << endl;
         cout << "***************" << endl;
         cout << "** Don't Buy **" << endl; 
         cout << "***************" << endl;
         return 0;
     }
+
+
+
+
+
+// Level 4
+
+    int number;
+    cout << "How much money can you spend for the food annually?" << endl;
+    cout << "1. under $150" << endl;
+    cout << "2. $150 - $300" << endl;
+    cout << "3. over $300" << endl;
+    cin >> number;
+
+
+    while ( number < 1 || number > 3) 
+    {
+        cout << endl;
+        cout << "Choose a number: " << endl;
+        cin >> number;
+    }
+
+    if (number == 1) 
+    {
+    cout << "You spend under $150 annually." << endl;
+    }
+
+        else if ( number == 2) {
+        cout << "You spend between $150 and $300 annually." << endl;
+        }
+
+        else if ( number == 3) {
+        cout << "You spend over $300 annually." << endl;    
+        }
+    
+        else {
+
+        cout << "Try one more time";
+
+        }
+    
+
+    cout << endl;
+    cout << "************" << endl;
+    cout << "****    ****" << endl;
+    cout << "** Buy it **" << endl; 
+    cout << "****    ****" << endl;
+    cout << "************" << endl;
 
     return 0;
 }
